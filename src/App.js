167af6861e93b26ef2c1b12e052a9f2b56ba8cc5
Abdallah Qaradeh <abdallahsqaradeh@gmail.com>
2020-10-14 17:02:23 +0300

    add styles to components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React, { Component } from "react";
 import "./App.css";
 import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/searchBox/searchBox.component"
 class App extends Component {
-  state = {
+  constructor()
+  {
+    super ();
+    this.state = {
     monsters: [],
     searchField: "",
   };
+  this.onchangeHandleronchangeHandler = this.onchangeHandler.bind(this);
+  }
+  
+  onchangeHandler = (e)=>
+  {
+    this.setState({ searchField: e.target.value }, () => {
+      console.log(this.state);
+    });
+  }
   render() {
+    const {searchField, monsters} = this.state;
+    const filteredMonsters = monsters.filter((monster, index)=>
+    {
+     return  monster.name.toLowerCase().includes(searchField.toLowerCase())
+    })
+
     return (
       <div className="App">
-        <input
-          type="search"
-          placeholder="Searc monsters"
-          onChange={(event) => {
-            this.setState({ searchField: event.target.value }, () => {
-              console.log(this.state);
-            });
-          }}
-        />
-        <CardList monsters={this.state.monsters} />
+        <h1>Monsters Rolodex</h1>
+        <SearchBox placeholder="Search monsters" handleChange={this.onchangeHandler}/>
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
